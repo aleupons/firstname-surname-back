2efcef3e05f82c2087fa27e03ede62bf1a576e38
Aleu <apahoquei@gmail.com>
2024-10-25 00:57:17 +0200

    Afegir permetre CORS al frontend de Render

--- server/index.js
+++ server/index.js
@@ -20,6 +20,26 @@ const serverStart = () => {
   app.use(cors());
   app.use(express.json());
 
+  app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://firstname-surname-front.onrender.com/"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    res.setHeader("Access-Control-Max-Age", 7200);
+
+    next();
+  });
+
   app.use("/users", usersRoute);
   app.use("/dibuixos", dibuixosRoute);
   app.use("/pintures", pinturesRoute);
