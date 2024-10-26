7c01f3233db868db88bdfdd468a5fb80d2222e46
Aleu <apahoquei@gmail.com>
2024-10-26 20:51:02 +0200

    CORS dev

--- server/index.js
+++ server/index.js
@@ -22,25 +22,25 @@ const serverStart = () => {
   app.use(express.json());
 
   /* Dev CORS */
-  // app.use((req, res, next) => {
-  //   res.setHeader(
-  //     "Access-Control-Allow-Origin",
-  //     "https://firstname-surname-front.onrender.com"
-  //   );
-  //   res.setHeader(
-  //     "Access-Control-Allow-Methods",
-  //     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  //   );
-  //   res.setHeader(
-  //     "Access-Control-Allow-Headers",
-  //     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  //   );
-  //   res.setHeader("Access-Control-Allow-Credentials", true);
-  //   res.setHeader("Access-Control-Allow-Private-Network", true);
-  //   res.setHeader("Access-Control-Max-Age", 7200);
+  app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://firstname-surname-front.onrender.com"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    res.setHeader("Access-Control-Max-Age", 7200);
 
-  //   next();
-  // });
+    next();
+  });
   /* */
 
   app.use("/users", usersRoute);
